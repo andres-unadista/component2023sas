8770e314159bd44be07f2124c540bf48a425d2c4
andres-unadista <affernandez999@gmail.com>
2023-04-04 23:29:42 -0500

    add logic utilities page

---
+++ js/utilities.js
@@ -0,0 +1,33 @@
+let $formSale = document.getElementById('formSale');
+let $btnClean = document.getElementById('clean');
+let $viewSale = document.getElementById('viewSale');
+let $viewProfit = document.getElementById('viewProfit');
+let $viewIVA = document.getElementById('viewIVA');
+let myModal = new bootstrap.Modal(document.getElementById('modaySale'), {
+  keyboard: false
+});
+
+$formSale.addEventListener('submit', handleSubmit);
+$btnClean.addEventListener('click', handleClean);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let formSale = new FormData($formSale);
+  let purchase = +formSale.get('purchase');
+  let profit = +formSale.get('profit') / 100;
+  let iva = +formSale.get('iva') / 100;
+  let sale = 0;
+
+  iva = purchase * iva;
+  profit = purchase * profit;
+  sale = iva + profit + purchase;
+
+  $viewSale.innerText = sale;
+  $viewProfit.innerText = profit;
+  $viewIVA.innerText = iva;
+  myModal.show();
+}
+
+function handleClean() {
+  $formSale.reset();
+}
